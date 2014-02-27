03da45e453f5cdd98fe35a988d22eb3a5df0baf5
Lauren Varney <l_varney_14@hotmail.com>
2014-02-26 16:01:30 -0800

    version 0

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,18 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
+	//Good Stuff goes here(main body)
+
+	new Maplace({
+		show_markers: true,
+		locations: [{
+			lat: 47.637703, 
+			lon: -117.3854611,
+			zoom: 15
+		}]
+	}).Load(); 
+
+
 	console.log('Page Loaded. Lets Do this!');
 
 }); 
